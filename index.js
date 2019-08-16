5db17a1c11c1f7c12409242fdf3042b117654ced
Christian Williams <chrisawill05@gmail.com>
2019-08-16 12:35:28 -0500

    Completed assignment Morning9

---
+++ index.js
@@ -0,0 +1,36 @@
+// Put the start of your program in a main function. Create a class for Books with name, rating, genre, and author. Create 3 instances of Books and put them in an array. Afterwards print each one in the format below:
+//     ```
+// The book [BOOK NAME] has [RATING] stars!!!
+// ```
+
+class books
+{
+    constructor(name,rating,genre)
+    {
+        this.name = name;
+        this.rating = rating;
+        this.genre = genre;
+    }
+    
+}
+
+function main() 
+{
+    let novel1 = new books("The Good Night","99.9%","Children's");
+    let novel2 = new books("Kudapual","99.9","Supernatural, slice-of-life,");
+    let novel3 = new books("Where the Ink Flows, There I Shall Go.","99.9","poetry");
+    
+    let arrayOfTitles = ["There is No Dog","The Knife of Never Letting Go", "The Crimson Crown" ];
+   
+    arrayOfTitles.forEach(function (element)
+    {
+        console.log(element)
+    })
+    
+    // arrayOfTitles.forEach(element);
+    // {
+    //     console.log(element)
+    // }
+
+}
+main();
